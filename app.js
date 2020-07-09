3f6a021d6599d3d0264b91f6bfcea9960d4efa1d
Deepak joshi <deepakjoshi0123@gmail.com>
2020-07-09 18:23:46 +0530

    socket bug fixed

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ io.on("connect", (socket) => {
   socket.on("join", ({ email, room }, callback) => {
     // console.log(" err - > ", email, room);
     const { error, user } = addUser({ id: socket.id, email, room });
-    // console.log(socket.id, " right or wrong user : ", user);
+    console.log(socket.id, " to check : ", user);
     if (error) return callback(error);
     socket.join(user.room);
     socket.emit("message", {
@@ -54,7 +54,8 @@ io.on("connect", (socket) => {
   //__________________________for one to one message______________________________________
 
   socket.on("1to1message", ({ message, user, usrfrnd }) => {
-    console.log("is it same ", getSocketId(usrfrnd)[0].id);
+    console.log("userfrnd", usrfrnd);
+    console.log("is it same ", usrfrnd, getSocketId(usrfrnd)[0].id);
     socket.to(getSocketId(usrfrnd)[0].id).emit("1to1message", {
       user: user,
       msg: message,
@@ -62,17 +63,14 @@ io.on("connect", (socket) => {
     });
   });
 
-  socket.on("acceptfrndreq", (usrfrnd) => {
-    console.log("yesemitted", usrfrnd);
-    console.log("why not going", getSocketId(usrfrnd)[0].id);
-    socket.to(getSocketId(usrfrnd)[0]).emit("acceptfrndreq", {
-      usrfrnd: usrfrnd,
+  socket.on("acceptfrndreq", (email, usrfrnd) => {
+    console.log("yesemitted", email, usrfrnd);
+    console.log("How 's it's same ", usrfrnd, getSocketId(usrfrnd)[0].id);
+    socket.to(getSocketId(usrfrnd)[0].id).emit("acceptfrndreq", {
+      // now have one solution but its not good w.r.to security
+      usrfrnd: email,
     });
   });
-
-  // socket.on("sendMessagePVT", (message, sendby, sento) => {
-  //   socket.to(getSocketId(sentto)).emit({ message: message });
-  // });
 
   //__________________________________________________________________________________________________
 
@@ -119,6 +117,7 @@ io.on("connect", (socket) => {
         user: "Admin",
         text: `${user.email} has left.`,
         addto: user.room,
+        time: new Date(new Date().getTime()).toLocaleTimeString(),
       });
       io.to(user.room).emit("roomData", {
         room: user.room,
